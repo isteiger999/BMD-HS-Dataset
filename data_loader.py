need to fix data leakage between train, val, test

data_loader.py
@@ -112,25 +112,37 @@ def filter(wav_file):
     output = torch.tensor(output_copy)
     return output
 
-def load_pcg_data(device):
-    X = torch.zeros([108, 8, 80000], dtype=torch.float32, device=device)
+def window_split(X, wav_file, win_len, stride, nr_windows, row, index_file):
+    wav_file = torch.tensor(wav_file)
+    for window in range(nr_windows):
+        X[row, index_file*nr_windows+window, :] = wav_file[window*stride:window*stride + win_len]
+    
+    return X
+
+def load_pcg_data(device, win_len, stride):
+
+    len_rec = 80000
+    nr_channels = 8
+    nr_windows = (len_rec - win_len) // stride + 1
+
+    X = torch.zeros([108, nr_channels*nr_windows, win_len], dtype=torch.float32, device=device)
     y = torch.zeros([108, 5], dtype=torch.float32, device=device)
 
     train_csv = pd.read_csv('data/train.csv')
     train = train_csv.to_numpy()
 
     for row in range(train_csv.shape[0]):
-        for index, file_name in enumerate(train_csv.iloc[row, 6:]):
+        for index_file, file_name in enumerate(train_csv.iloc[row, 6:]):
             wav_file, _ = librosa.load(f'data/train/{file_name}.wav', sr=4000)
             if wav_file.shape[0] != 80000:
                 wav_file = fix_length(wav_file)
 
             #wav_file_filtered = filter(wav_file)    
-            X[row, index, :] = wav_file
+            X = window_split(X, wav_file, win_len, stride, nr_windows, row, index_file)
         
         labels = train[row, 1:6]
         labels = torch.tensor(labels.astype(float), dtype=torch.float32)
         labels = labels.view(1, -1)
         y[row, :] = labels
 
-    return X, y
+    return X, y, nr_windows

main.py
@@ -1,14 +1,18 @@
 import torch
 from data_loader import load_data_simple, split_data, loaders, mean_std, load_pcg_data
 from models.ANN import ANN, train_ann, test_ann
-from models.transformer import Transformer, train_transformer
+from models.transformer import Transformer, train_transformer, test_transformer
 
 def main():
     device = torch.device("mps" if torch.mps.is_available() else "cpu")
     metrics = {"Final_loss": [], "Acc": []}
 
     #X, y = load_data_simple()
-    X, y = load_pcg_data(device)
+    win_len = 8000
+    stride = 2000
+    X, y, nr_windows = load_pcg_data(device, win_len, stride)
+    print(X.shape)
+    print(y.shape)
 
     iterations = 5
     stride_splits = 1/iterations
@@ -17,14 +21,17 @@ def main():
         X_train, y_train, X_val, y_val, X_test, y_test = split_data(X, y, iteration, stride_splits, split=[0.70, 0.15, 0.15])
         train_loader, val_loader, test_loader = loaders(X_train, y_train, X_val, y_val, X_test, y_test)
         
-        transformer = Transformer().to(device)
+        transformer = Transformer(nr_windows, win_len).to(device)
         train_transformer(transformer, train_loader, val_loader, device, epochs = 150)
+        test_transformer(transformer, val_loader, device, metrics)
+        
         '''
         ann = ANN().to(device)
         train_ann(ann, device, train_loader, val_loader)
         metrics = test_ann(ann, device, val_loader, test_loader, metrics, mode="val") # mode = "val" vs. "test"
         '''
-    #mean_std(metrics)
+    
+    mean_std(metrics)
 
 if __name__ == '__main__':
     main()

models/transformer.py
@@ -6,19 +6,18 @@ import math
 import copy
 
 num_channels = 8
-embed_dim = 100
-kernel_size = 1000 # 4000 = 1sec
+embed_dim = 64
+kernel_size = 100 # 4000 = 1sec
 stride = kernel_size//4
 num_classes = 5
 attention_heads = 4
-transformer_blocks = 4
+transformer_blocks = 2 
 mlp_nodes = 256
-nr_tokens = (80000 - kernel_size) // stride + 1
 
 class PatchEmbedding(nn.Module):
-    def __init__(self):
+    def __init__(self, nr_windows):
         super().__init__()
-        self.patch_embed = nn.Conv1d(in_channels=num_channels, out_channels=embed_dim, kernel_size=kernel_size, stride = stride)
+        self.patch_embed = nn.Conv1d(in_channels=8*nr_windows, out_channels=embed_dim, kernel_size=kernel_size, stride = stride)
 
     def forward(self, x):
         embedding = self.patch_embed(x)             # outputs [batch, embed_dim, #tokens]
@@ -30,17 +29,19 @@ class TransformerEncoder(nn.Module):
         super().__init__()
         self.ln1 = nn.LayerNorm(embed_dim)
         self.multi_head_attention = nn.MultiheadAttention(embed_dim, attention_heads, batch_first=True)
+        self.dropout = nn.Dropout1d(p=0.2)
         self.ln2 = nn.LayerNorm(embed_dim)
         self.mlp = nn.Sequential(
             nn.Linear(embed_dim, mlp_nodes),
             nn.GELU(),
+            nn.Dropout(p=0.2),
             nn.Linear(mlp_nodes, embed_dim)
         )
 
     def forward(self, x):
         residual1 = x
         x = self.ln1(x)
-        x = self.multi_head_attention(x, x, x)[0] + residual1
+        x = self.dropout(self.multi_head_attention(x, x, x)[0]) + residual1
         residual2 = x
         x = self.ln2(x)
         x = self.mlp(x) + residual2
@@ -50,20 +51,24 @@ class MLP_Head(nn.Module):
     def __init__(self):
         super().__init__()
         self.ln1 = nn.LayerNorm(embed_dim)          # takes in cls token only
+        self.dropout = nn.Dropout(p=0.2)
         self.fc1 = nn.Linear(embed_dim, mlp_nodes)
         self.fc2 = nn.Linear(mlp_nodes, mlp_nodes)
         self.fc3 = nn.Linear(mlp_nodes, num_classes)
 
     def forward(self, x):
         x = F.relu(self.fc1(self.ln1(x)))
+        x = self.dropout(x)
         x = F.relu(self.fc2(x))
+        x = self.dropout(x)
         x = self.fc3(x)
         return x
     
 class Transformer(nn.Module):
-    def __init__(self):
+    def __init__(self, nr_windows, win_len):
+        nr_tokens = (win_len - kernel_size) // stride + 1
         super().__init__()
-        self.embedding = PatchEmbedding()
+        self.embedding = PatchEmbedding(nr_windows)
         self.cls_token = nn.Parameter(torch.randn(1,1,embed_dim))
         self.position_embedding = nn.Parameter(torch.randn(1, nr_tokens+1, embed_dim))
         self.transformer_block = nn.Sequential(*[TransformerEncoder() for _ in range(transformer_blocks)])
@@ -82,9 +87,15 @@ class Transformer(nn.Module):
 
 
 def train_transformer(transformer, train_loader, val_loader, device, epochs = 150):
-    optimizer = optim.Adam(transformer.parameters(), lr = 5e-4, weight_decay=1e-4)
+    optimizer = optim.Adam(transformer.parameters(), lr = 5e-4, weight_decay=1e-3)
     criterion_train = nn.BCEWithLogitsLoss()
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5, factor=0.5, min_lr=5e-6)
+
+    # for early stopping
+    early_st_patience = 10
+    best_val = math.inf
+    bad_epochs = 0
+    best_state = None
 
     for epoch in range(epochs):
         transformer.train()
@@ -110,6 +121,7 @@ def train_transformer(transformer, train_loader, val_loader, device, epochs = 15
         train_loss /= total_train
         train_acc = correct_train / total_train
 
+        # lr scheduler
         transformer.eval()
         criterion_val = nn.BCEWithLogitsLoss()
         total_val, correct_val = 0, 0
@@ -133,12 +145,8 @@ def train_transformer(transformer, train_loader, val_loader, device, epochs = 15
         scheduler.step(val_loss)
 
         # early stopping
-        early_st_patience = 8
-        best_val = -math.inf
-        bad_epochs = 0
-        best_state = None
-        if val_acc > best_val:
-            best_val = val_acc
+        if val_loss < best_val:
+            best_val = val_loss
             bad_epochs = 0
             best_state = copy.deepcopy(transformer.state_dict())
         else:
@@ -148,4 +156,34 @@ def train_transformer(transformer, train_loader, val_loader, device, epochs = 15
                     transformer.load_state_dict(best_state)
                 break
 
-        if epoch%10==0: print(f"Epoch {epoch}: train_acc: {train_acc} || train_loss: {train_loss} || val_acc: {val_acc} || val_loss: {val_loss}, lr: {optimizer.param_groups[0]['lr']:.6f}")
+        print(f"Epoch {epoch}: train_acc: {round(train_acc, 2)} || train_loss: {round(train_loss, 3)} || val_acc: {round(val_acc, 2)} || val_loss: {round(val_loss, 3)}, lr: {optimizer.param_groups[0]['lr']:.6f}")
+
+def test_transformer(transformer, val_loader, device, metrics):
+    transformer.eval()
+    criterion_val = nn.BCEWithLogitsLoss()
+    total_val, correct_val = 0, 0
+    val_loss = 0
+
+    with torch.no_grad():
+        for xv, yv in val_loader:
+            xv, yv = xv.to(device), yv.to(device)
+            pred = transformer(xv)
+            loss = criterion_val(pred, yv)
+            val_loss += loss.item()
+            y_pred = torch.sigmoid(pred)
+            y_pred = (y_pred > 0.5).int()
+            
+            total_val += xv.shape[0]
+            for row in range(xv.shape[0]):
+                if torch.equal(y_pred[row, :], yv[row, :]):
+                    correct_val += 1
+                
+    val_loss /= total_val
+    val_acc = correct_val / total_val
+
+    metrics["Final_loss"].append(val_loss)
+    metrics["Acc"].append(val_acc)
+
+    return metrics
+
+